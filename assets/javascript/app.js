ce53272073223af789b679663e1ac2723bffb1f9
Zander Dyer <zanderdyer@Zanders-MacBook-Pro.local>
2018-12-22 11:57:48 -0700

    initialize firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCgvMG6X-qUQOMJUNoOq6jOIxeIiS5dOVs",
+    authDomain: "project1-fae24.firebaseapp.com",
+    databaseURL: "https://project1-fae24.firebaseio.com",
+    projectId: "project1-fae24",
+    storageBucket: "project1-fae24.appspot.com",
+    messagingSenderId: "57183397397"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+
