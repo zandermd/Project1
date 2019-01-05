bd6b5f627a2d192c75a8a2c03ab6ab5b8408c3fc
bowieisagod <jeremygriddell@gmail.com>
2019-01-05 11:42:21 -0700

    push1519A

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,54 +11,57 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 // BandsInTown AJAX - API
-{
-    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {
-    
-          // Printing the entire object to console
-          console.log(response);
-    
-          // Constructing HTML containing the artist information
-          var artistName = $("<band>").text(response.name);
-          var artistURL = $("<band>").attr("href", response.url).append(artistName);
-          var artistImage = $("<img>").attr("src", response.thumb_url);
-          var upcomingEvents = $("<h2>").text(response.upcoming_event_count + " upcoming events");
-          
-    
-          // Empty the contents of the band-div, append the new artist content
-          $("#band-div").empty();
-          $("#band-div").append(artistURL, artistImage, trackerCount, upcomingEvents, goToArtist);
-        });
-      }
-    
-      // Event handler for user clicking the stalk button
-      $("#stalk").on("click", function(event) {
-        // Preventing the button from trying to submit the form
-        event.preventDefault();
-        // Storing the artist name
-        var inputArtist = $("#artist-input").val().trim();
-    
-        // Running the searchBandsInTown function(passing in the artist as an argument)
-        searchBandsInTown(inputArtist);
-      });
+function searchBandsInTown(artist) {
+    //  var queryURL = "https://rest.bandsintown.com/artists/Muse?app_id=codingbootcamp";
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp&date=upcoming";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
 
-      //Youtube video finder AJAX - API
+        // Printing the entire object to console
+        console.log(response);
+
+        // Constructing HTML containing the artist information
+        var artistName = $("<band>").text(response.name);
+        var artistURL = $("<band>").attr("href", response.url).append(artistName);
+        var artistImage = $("<img>").attr("src", response.thumb_url);
+        var upcomingEvents = $("<h2>").text(response.upcoming_event_count + " upcoming events");
+
+
+        // Empty the contents of the band-div, append the new artist content
+        $("#band-div").empty();
+        $("#band-div").append(artistURL, artistImage, upcomingEvents, goToArtist);
+    });
+}
+
+// Event handler for user clicking the stalk button
+$("#stalk").on("click", function (event) {
+    console.log("button clicked");
+    // Preventing the button from trying to submit the form
+    event.preventDefault();
+    // Storing the artist name
+    var inputArtist = $("#artist-input").val().trim();
+    $(".jumbotron").hide();
+$("#info").show();
+    // Running the searchBandsInTown function(passing in the artist as an argument)
+    searchBandsInTown(inputArtist);
+});
+
+//Youtube video finder AJAX - API
 
 var videoArtist;
 
-$("#search-btn").on("click", function() {
-    videoArtist =$("#query").val();
+$("#search-btn").on("click", function () {
+    videoArtist = $("#query").val();
 });
 
 
 var gapikey = 'AIzaSyAT2Wav_q-sVdSMNwogqaJtlJy0NQiYpsU';
 
-$(function() {
+$(function () {
 
-    $('#search-form').submit( function(e) {
+    $('#search-form').submit(function (e) {
         e.preventDefault();
     });
 
@@ -69,7 +72,7 @@ function search(artist) {
     // clear 
     $('#results').html('');
     $('#buttons').html('');
-    
+
     // get form input
     q = $('#query').val();
 
@@ -77,107 +80,107 @@ function search(artist) {
         method: 'GET',
         url: `https://www.googleapis.com/youtube/v3/search?&part=snippet,id&q=${artist}&type=video&key=${gapikey}`,
         headers: 'Access-Control-Allow-Origin'
-    }).done((data)=>{
+    }).done((data) => {
         console.log(data);
         var nextPageToken = data.nextPageToken;
-            var prevPageToken = data.prevPageToken;
-            
-            // Log data
-            console.log(data);
-            
-            $.each(data.items, function(i, item) {
-                
-                // Get Output
-                var output = getOutput(item);
-                
-                // display results
-                $('#results').append(output);
-            });
-            
-            var buttons = getButtons(prevPageToken, nextPageToken);
-            
-            // Display buttons
-            $('#buttons').append(buttons);
-    });
- };
-    
-    
-    // clear 
-    $('#results').html('');
-    $('#buttons').html('');
-    
-    // get form input
-    q = $('#query').val();  
+        var prevPageToken = data.prevPageToken;
 
-    // run get request on API
-    $.get(
-        `https://www.googleapis.com/youtube/v3/search?&part=snippet,id&q=${videoArtist}&type=video&key=${gapikey}`, {
-            part: 'snippet, id',
-            q: q,
-            pageToken: token,
-            type: 'video',
-            key: gapikey
-        }, function(data) {
-            
-            var nextPageToken = data.nextPageToken;
-            var prevPageToken = data.prevPageToken;
-            
-            // Log data
-            console.log(data);
-            
-            $.each(data.items, function(i, item) {
-                
-                // Get Output
-                var output = getOutput(item);
-                
-                // display results
-                $('#results').append(output);
-            });
-            
-            var buttons = getButtons(prevPageToken, nextPageToken);
-            
-            // Display buttons
-            $('#buttons').append(buttons);
-        });    
-    
-    
-    // clear 
-    $('#results').html('');
-    $('#buttons').html('');
-    
-    // get form input
-    q = $('#query').val();
-    
-    // run get request on API
-    $.get(
-        `https://www.googleapis.com/youtube/v3/search?&part=snippet,id&q=${videoArtist}&type=video&key=${gapikey}`, {
-            part: 'snippet, id',
-            q: q,
-            pageToken: token,
-            type: 'video',
-            key: gapikey
-        }, function(data) {
-            
-            var nextPageToken = data.nextPageToken;
-            var prevPageToken = data.prevPageToken;
-            
-            // Log data
-            console.log(data);
-            
-            $.each(data.items, function(i, item) {
-                
-                // Get Output
-                var output = getOutput(item);
-                
-                // display results
-                $('#results').append(output);
-            });
-            
-            var buttons = getButtons(prevPageToken, nextPageToken);
-            
-            // Display buttons
-            $('#buttons').append(buttons);
-        });    
+        // Log data
+        console.log(data);
+
+        $.each(data.items, function (i, item) {
+
+            // Get Output
+            var output = getOutput(item);
+
+            // display results
+            $('#results').append(output);
+        });
+
+        var buttons = getButtons(prevPageToken, nextPageToken);
+
+        // Display buttons
+        $('#buttons').append(buttons);
+    });
+};
+
+
+// clear 
+$('#results').html('');
+$('#buttons').html('');
+
+// get form input
+q = $('#query').val();
+
+// run get request on API
+// $.get(
+//     `https://www.googleapis.com/youtube/v3/search?&part=snippet,id&q=${videoArtist}&type=video&key=${gapikey}`, {
+//         part: 'snippet, id',
+//         q: q,
+//         pageToken: token,
+//         type: 'video',
+//         key: gapikey
+//     }, function(data) {
+
+//         var nextPageToken = data.nextPageToken;
+//         var prevPageToken = data.prevPageToken;
+
+//         // Log data
+//         console.log(data);
+
+//         $.each(data.items, function(i, item) {
+
+//             // Get Output
+//             var output = getOutput(item);
+
+//             // display results
+//             $('#results').append(output);
+//         });
+
+//         var buttons = getButtons(prevPageToken, nextPageToken);
+
+//         // Display buttons
+//         $('#buttons').append(buttons);
+//     });    
+
+
+// clear 
+$('#results').html('');
+$('#buttons').html('');
+
+// get form input
+q = $('#query').val();
+
+// run get request on API
+// $.get(
+//     `https://www.googleapis.com/youtube/v3/search?&part=snippet,id&q=${videoArtist}&type=video&key=${gapikey}`, {
+//         part: 'snippet, id',
+//         q: q,
+//         pageToken: token,
+//         type: 'video',
+//         key: gapikey
+//     }, function(data) {
+
+//         var nextPageToken = data.nextPageToken;
+//         var prevPageToken = data.prevPageToken;
+
+//         // Log data
+//         console.log(data);
+
+//         $.each(data.items, function(i, item) {
+
+//             // Get Output
+//             var output = getOutput(item);
+
+//             // display results
+//             $('#results').append(output);
+//         });
+
+//         var buttons = getButtons(prevPageToken, nextPageToken);
+
+//         // Display buttons
+//         $('#buttons').append(buttons);
+//     });    
 
 
 // Build output
@@ -189,37 +192,37 @@ function getOutput(item) {
     var thumb = item.snippet.thumbnails.high.url;
     var channelTitle = item.snippet.channelTitle;
     var videoDate = item.snippet.publishedAt;
-    
+
     // Build output string
-    var output = 	'<li>' +
-                        '<div class="list-left">' +
-                            '<img src="' + thumb + '">' +
-                        '</div>' +
-                        '<div class="list-right">' +
-                            '<h3><a data-fancybox-type="iframe" class="fancyboxIframe" href="https://youtube.com/embed/' + videoID + '?rel=0" target="new">' + title + '</a></h3>' +
-                            '<small>By <span class="cTitle">' + channelTitle + '</span> on ' + videoDate + '</small>' +
-                            '<p>' + description + '</p>' +
-                        '</div>' +
-                    '</li>' +
-                    '<div class="clearfix"></div>' +
-                    '';
+    var output = '<li>' +
+        '<div class="list-left">' +
+        '<img src="' + thumb + '">' +
+        '</div>' +
+        '<div class="list-right">' +
+        '<h3><a data-fancybox-type="iframe" class="fancyboxIframe" href="https://youtube.com/embed/' + videoID + '?rel=0" target="new">' + title + '</a></h3>' +
+        '<small>By <span class="cTitle">' + channelTitle + '</span> on ' + videoDate + '</small>' +
+        '<p>' + description + '</p>' +
+        '</div>' +
+        '</li>' +
+        '<div class="clearfix"></div>' +
+        '';
     return output;
 };
 
 function getButtons(prevPageToken, nextPageToken) {
-    if(!prevPageToken) {
-        var btnoutput = 	'<div class="button-container">' +
-                                '<button id="next-button" class="paging-button" data-token="' + nextPageToken + '" data-query="' + q + '"' +
-                                    'onclick = "nextPage();">Next Page</button>' +
-                            '</div>';
+    if (!prevPageToken) {
+        var btnoutput = '<div class="button-container">' +
+            '<button id="next-button" class="paging-button" data-token="' + nextPageToken + '" data-query="' + q + '"' +
+            'onclick = "nextPage();">Next Page</button>' +
+            '</div>';
     } else {
-        var btnoutput = 	'<div class="button-container">' +
-                                '<button id="prev-button" class="paging-button" data-token="' + prevPageToken + '" data-query="' + q + '"' +
-                                    'onclick = "prevPage();">Prev Page</button>' +            
-                                '<button id="next-button" class="paging-button" data-token="' + nextPageToken + '" data-query="' + q + '"' +
-                                    'onclick = "nextPage();">Next Page</button>' +
-                            '</div>';        
+        var btnoutput = '<div class="button-container">' +
+            '<button id="prev-button" class="paging-button" data-token="' + prevPageToken + '" data-query="' + q + '"' +
+            'onclick = "prevPage();">Prev Page</button>' +
+            '<button id="next-button" class="paging-button" data-token="' + nextPageToken + '" data-query="' + q + '"' +
+            'onclick = "nextPage();">Next Page</button>' +
+            '</div>';
     }
-    
+
     return btnoutput;
 };
